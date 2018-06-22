e297bbe33424df54712511647ab320b99c56e861
Kirill Varlamov <kirill@ongrid.pro>
2018-06-22 19:17:04 +0300

    Babelize truffle

--- truffle.js
+++ truffle.js
@@ -12,6 +12,10 @@
  *   },
  */
 
+require('dotenv').config();
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
